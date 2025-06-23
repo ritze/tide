2b8270f5c603344c61ea8dd2097405dfc2b118e1
Moritz Luedecke <moritz.luedecke@wibu.com>
2025-06-23 09:15:09 +0200

    Remove documentation and tests

--- tests/test_setup.fish
+++
@@ -1,8 +0,0 @@
-function _tide_decolor
-    string replace --all -r '\e(\[[\d;]*|\(B\e\[)m(\co)?' '' "$argv"
-end
-funcsave _tide_decolor
-
-echo "\
-set TERM xterm-256color
-set -g _tide_side right" >$__fish_config_dir/conf.d/tide_test_setup.fish
--- tools/track_dimensions.fish
+++
@@ -1,3 +0,0 @@
-function track_dimensions --on-signal WINCH
-    echo $COLUMNS x $LINES
-end
